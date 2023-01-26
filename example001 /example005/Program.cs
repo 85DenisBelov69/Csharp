353877838a0d0b452ddca042af31c1395f317c9c
DenisBelov <olgabelova@192.168.0.4>
2023-01-26 15:01:23 +0700

    add Tolower

--- example001 /example005/Program.cs
+++ example001 /example005/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура, это же Маша!");
+
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+
+}
